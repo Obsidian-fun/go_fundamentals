88211d19385542fbdfd818e2b5635196102e83aa
Obsidian <obsidiancrystal@tutanota.com>
2024-05-18 17:31:32 +0530

    new file:   EchoServer.go

---
+++ EchoServer.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln("Please specify an address");
+	}
+
+	addr, err := net.ResolveTCPAddr("tcp",os.Args[1];
+	if err != nil {
+		log.Fatalln("Invalid addres: ",os.Args[1], err);
+	}
+
+	listener, err := net.ListenTCP("tcp", addr);
+	if err != nil {
+		log.Fatalln("Listener: ",os.Args[1], err);
+	}
+	log.Println("Listening on ", addr);
+
+	go createConn(addr);
+
+
+
+}
+
+

fa5c76882cd24be0d0f428fa872b26ec1837cad3
Obsidian <obsidiancrystal@tutanota.com>
2024-05-18 22:17:46 +0530

    new file:   client.go 	new file:   server.go

---
+++ client-server/server.go
@@ -0,0 +1,27 @@
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+	"net"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("usage: ./server [address:port]");
+	}
+
+	addr, err := net.ResolveTCPAddr("tcp", os.Args[1]);
+	if err != nil {
+		log.Println("address in use ", addr, err);
+	}
+
+	
+
+
+
+}

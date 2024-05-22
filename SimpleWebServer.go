74d8a95e45803287920967c3ecb6aab67b6b92b8
Obsidian <obsidiancrystal@tutanota.com>
2024-05-23 01:48:06 +0530

    modified:   SimpleWebServer.go

--- SimpleWebServer.go
+++ SimpleWebServer.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -14,23 +15,26 @@ func (c *CustomHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
 }
 
 func main() {
-	mux := http.NewServerMux();
+	mux := http.NewServeMux();
 
 	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w,"Hello!");
 	});
 
-	mux.HandleFunc("/bye", func(w http.ResponseWriter, r *Request){
+	mux.HandleFunc("/bye", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w,"Goodbye.");
 	});
 
-	mux.HandleFunc("/error", func(w http.ResponseWriter, r *Request){
+	mux.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request){
 		w.WriteHeader(http.StatusInternalServerError);
 		fmt.Fprintf(w,"An Error occured [501]");
 	});
 
-	mux.HandleFunc("/custom", new(CustomHandler));
+	mux.Handle("/custom", new(CustomHandler));
 
+	if err := http.ListenAndServe(":8000", mux); err != nil {
+		log.Fatal(err);
+	}
 }
 
 

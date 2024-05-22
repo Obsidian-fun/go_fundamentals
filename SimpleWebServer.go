205266f1a2b0d1a4a90b7e71ea7115c29a9da8e2
Obsidian <obsidiancrystal@tutanota.com>
2024-05-23 00:35:46 +0530

    modified:   SimpleWebServer.go

--- SimpleWebServer.go
+++ SimpleWebServer.go
@@ -3,11 +3,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
 	"net/http"
 )
 
 type logTripper struct {
 	http.RoundTripper;
+}
+
+func (l logTripper) logger(r *http.Request) (*http.Response, error)(
+	log.Println(r.URL);
+	r.Header.Set("X-Log-Time", time.Now().String());
+
 }
 
 func main() {

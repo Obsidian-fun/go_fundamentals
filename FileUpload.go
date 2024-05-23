f8e11abfb6a7dcc44afc97dce69c13b57c858a0a
Obsidian-fun <obsidiancrystal@tutanota.com>
2024-05-24 00:06:36 +0530

    modified:   FileUpload.go

--- FileUpload.go
+++ FileUpload.go
@@ -14,11 +14,16 @@ func main() {
 	const (
 		param = "file";
 		endpoint = "/uplaod";
-		content = <html lang="en">
-              <body>
-                <form action="%s" enctype="multipart/form-data" method="POST">
-                    <input type="text" name="%s">
-                    <input type="submit" value="Upload">
-                </form>  
-              </body>
-              </html>
+		content = `<html lang="en">`+
+              `<body>`+
+                `<form action="%s" enctype="multipart/form-data" method="POST">`+
+                   `<input type="text" name="%s">`+
+                   `<input type="submit" value="Upload">`+
+               `</form>`+
+              `</body>`+
+              `</html>`
+
+
+
+
+

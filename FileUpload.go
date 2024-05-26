cb0bb1838ce9b34a5cadd76307f6eb1928e8460b
Obsidian <obsidiancrystal@tutanota.com>
2024-05-26 12:42:52 +0530

    modified:   FileUpload.go

--- FileUpload.go
+++ FileUpload.go
@@ -7,6 +7,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"io"
+	"os"
+	"path/filepath"
 	"net/http"
 )
 
@@ -24,12 +27,49 @@ import (
 	)
 
 	func main() {
-		mux := http.NewServerMux();
+		mux := http.NewServeMux();
 
 		mux.HandleFunc(endpoint, func(w http.ResponseWriter,r *http.Request){
 			if r.Method == "GET" {
-				
+				fmt.Fprintf(w, content, endpoint, param);
+			}else if r.Method == "POST" {
+					http.NotFound(w, r);
 			}
 
+			path, err := upload(r);
+			if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError);
+					return;
+			}
+			fmt.Fprintf(w, "Uploaded to %s",path);
+		});
+
+		if err := http.ListenAndServe(":8000",mux); err != nil{
+				log.Fatal(err);
+		}
+}
+
+func upload(r *http.Request) (string, error) {
+// Get file
+	f, h, err := r.FormFile(param);
+	if err != nil {
+		return "",err;
+	}
+	defer f.Close();
+
+// File write
+	p := filepath.Join(os.TempDir(), h.Filename);
+	fw, err := os.OpenFile(p, os.O_WRONLY | os.O_CREATE, 0666);
+	if err != nil{
+		return "", err;
+	}
+	defer fw.Close();
+
+// Copy written file to original file, updating it.
+	if _ , err := io.Copy(fw, f); err != nil {
+		return "",err;
+	}
+
+	return p, nil;
 }
 

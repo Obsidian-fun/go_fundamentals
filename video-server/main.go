2adc49d18d4439f497eee1c6d361ba03b450a04a
Obsidian <obsidiancrystal@tutanota.com>
2024-05-30 10:53:46 +0530

    new file:   index.html 	modified:   main.go

--- video-server/main.go
+++ video-server/main.go
@@ -10,18 +10,22 @@ import (
 func main() {
 
 	const dirName = "video-server";
-	const port = "5555";
+	const port = ":5555";
 
-	// addHeaders is a custom func, to bypass CORS
-	http.Handle("/",addHeaders
 
-}
+	http.Handle("/stream",addHeaders(http.FileServer(http.Dir(dirName))));
+	fmt.Printf("Serving on localhost%v\n",port);
 
-func addHeaders(h http.Handler) h.http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header("Access-Control-Allow-Origin","*");
-		h.ServeHttp(w, r);
+	err := (http.ListenAndServe(port,nil)); if err != nil {
+		log.Fatalln(err);
 	}
 }
 
+// addHeaders is a custom func, to bypass CORS
+func addHeaders(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin","*");
+		h.ServeHTTP(w, r);
+	}
+}
 

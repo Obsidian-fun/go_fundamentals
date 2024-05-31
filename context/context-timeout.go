16648c82d26381318d652cadd6968b8ea501388c
Obsidian <obsidiancrystal@tutanota.com>
2024-05-31 14:24:05 +0530

    new file:   context/context-timeout.go

---
+++ context/context-timeout.go
@@ -0,0 +1,28 @@
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"context"
+	"net/http"
+	"os"
+	"time"
+	"log"
+)
+
+func main() {
+
+// Create a new request
+
+	req, err := http.NewRequest()
+
+	ctx := context.Context;
+
+
+
+
+
+}
+
+

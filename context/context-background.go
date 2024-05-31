fea8f666f2d95bc37f3fa21061c1f51f78025c52
Obsidian <obsidiancrystal@tutanota.com>
2024-05-31 17:03:57 +0530

    new file:   context/.context-background.go.swp 	modified:   context/context-background.go

--- context/context-background.go
+++ context/context-background.go
@@ -1,4 +1,22 @@
 
 package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
+func main() {
+	ctx := context.Background();
+	done := ctx.Done();
+
+	for i:=0; ;i++ {
+		select {
+		case <-done:
+			return
+		case <-time.After(time.Second):
+			fmt.Println("tick ", i);
+		}
+	}
+}

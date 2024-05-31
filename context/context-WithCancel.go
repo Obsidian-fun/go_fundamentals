0c23fd223e27dff249a011b6ccacf8f51a37dfe6
Obsidian <obsidiancrystal@tutanota.com>
2024-05-31 17:19:25 +0530

    modified:   context-WithCancel.go

--- context/context-WithCancel.go
+++ context/context-WithCancel.go
@@ -1,4 +1,11 @@
+/***
+The context.WithCancel decorator function gets a context and returns another
+context and a function called cancel . The returned context will be a copy of
+the context that has a different done channel (the channel that marks that the
+current context is done) that gets closed when the parent context does or
+when the cancel function is called – whatever happens first.
 
+***/
 package main
 
 import (
@@ -9,5 +16,7 @@ import (
 
 func main() {
 
-	ctx := context.withCancel
+	ctx, cancel := context.withCancel(context.Background());
+	time.After(time.Second*5, done);
+	done := ctx.Done();
 

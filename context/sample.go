3d1d6449d9458ab2438abbedf0ed4943d63632f4
Obsidian <obsidiancrystal@tutanota.com>
2024-06-02 20:35:37 +0530

    modified:   sample.go

--- context/sample.go
+++ context/sample.go
@@ -33,9 +33,18 @@ func main() {
 
 	// Client-side,
 
-	ctx, cancel := context.Context
+	ctx, cancel := context.WithCancel(context.Background);
+	ch, o, wg := make([]chan int), sync.Once{}, sync.WaitGroup;
 
-
-
+	wg.Add(10);
+	for i:=0; i<10; i++ {
+		go func(int n) {
+			defer wg.Done();
+			req,_ := http.NewRequest("GET","http://"+address,nil);
+			if err := http.DefaultClient.Do(req.WithContext(ctx)); err != nil {
+				log.Fatalln(err);
+			}
+		}
+	}
 }
 

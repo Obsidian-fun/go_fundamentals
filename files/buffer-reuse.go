44ec667915cf1840d9068bf86e9af6b2fd87d986
Obsidian <obsidiancrystal@tutanota.com>
2024-06-05 13:11:26 +0530

    modified:   buffer-reuse.go

--- files/buffer-reuse.go
+++ files/buffer-reuse.go
@@ -3,31 +3,22 @@ package main
 
 import (
 	"fmt"
-	"bytes"
 	"log"
+	"bytes"
 	"os"
 )
 
 func main() {
 
-	if len(os.Args) != 2 {
-		log.Fatal("mention a file to read");
-	}
+	var buf := bytes.NewBuffer(make([]byte 26));
 
-	var text, err = os.Open(os.Args[1]);
-	if err != nil {
-		fmt.Println("file not found\n", err);
-		return
-	}
+	var texts = {
+				`This is a sentence,`
+				`What you see here, is also a sentence..`
+				`And yet another! Another sentence..`
+				`Guess what? You guessed it, a sentence.`
 
-	b := bytes.NewBuffer(make([]byte, 26));
 
-	defer text.Close();
-	for i:=0; ; i++ {
-		b.Reset();
-		n := text.Read(b);
-		fmt.Printf("%s",string(n[:i]));
-	}
+
+
 }
-
-

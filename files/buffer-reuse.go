37e49e261b358557580786b510fef12ac618b82a
Obsidian <obsidiancrystal@tutanota.com>
2024-06-05 13:32:02 +0530

    modified:   buffer-reuse.go

--- files/buffer-reuse.go
+++ files/buffer-reuse.go
@@ -3,22 +3,24 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"bytes"
-	"os"
 )
 
 func main() {
 
-	var buf := bytes.NewBuffer(make([]byte 26));
+	var buf = bytes.NewBuffer(make([]byte, 26));
 
-	var texts = {
-				`This is a sentence,`
-				`What you see here, is also a sentence..`
-				`And yet another! Another sentence..`
-				`Guess what? You guessed it, a sentence.`
+	var texts = []string{
+				`This is a sentence,`,
+				`What you see here, is also a sentence..`,
+				`And yet another! Another sentence..`,
+				`Guess what? You guessed it, a sentence.`,
+				}
 
+	for i:= range texts {
+		buf.Reset();
+		buf.WriteString(texts[i]);
+		fmt.Println("Length",buf.Len(), "\t capacity", buf.Cap());
 
-
-
+	}
 }

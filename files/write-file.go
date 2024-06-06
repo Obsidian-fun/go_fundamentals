608a067f83c7fe9d2a1a1ab92fdfaa8ff6b9afbd
Obsidian <obsidiancrystal@tutanota.com>
2024-06-07 01:07:47 +0530

    new file:   write-file.go

---
+++ files/write-file.go
@@ -0,0 +1,23 @@
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"io"
+)
+
+func main() {
+
+	if len(os.Args) != 3 {
+		log.Println("please specify a file name and text");
+		return
+	}
+
+	f := os.WriteFile(os.Args[1], []byte(os.Args[2]), 0644); err != nil {
+		fmt.Println("err: ",err);
+	}
+
+}
+

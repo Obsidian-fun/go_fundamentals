b9dab6b25ccf761dbd4a8b68716337c27cba7f42
Obsidian <obsidiancrystal@tutanota.com>
2024-06-12 11:22:17 +0530

    modified:   context-gg.go

--- context/context-gg.go
+++ context/context-gg.go
@@ -4,9 +4,15 @@ package main
 import (
 	"context"
 	"time"
+	"log"
+	"fmt"
 )
 
 func getUserData(ctx context.Context, userID int) (int, error) {
+
+	ctx, cancel := context.WithCancel(ctx, time.Millisecond*200);
+	defer cancel();
+
 	val, err := tooSlow();
 	if err != nil {
 		return 0, err;
@@ -23,7 +29,14 @@ func tooSlow() (int , error){
 
 func main() {
 
+	start := time.Now();
 	ctx := context.Background();
-	val, err := getUserData(ctx, )
+	userID := 10;
+	val, err := getUserData(ctx, userID);
+	if err != nil {
+		log.Fatal(err);
+	}
 
+	fmt.Println("result :", val);
+	fmt.Println(time.Since(start));
 }

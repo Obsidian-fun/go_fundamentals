4cd76c665acf76ec2335526515c6df28b4789a17
Obsidian <obsidiancrystal@tutanota.com>
2024-06-14 18:32:50 +0530

    new file:   channel-block2.go

---
+++ concurrency/channel-block2.go
@@ -0,0 +1,35 @@
+/***
+Continuing with the example, channel will ouput values thanks to the "suck", for about 5 seconds 
+before finishing the program.
+
+***/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// infinit loop,
+func pump(ch chan int) {
+	for i:=0; ; i++ {
+		ch <- i;
+	}
+}
+
+func suck(ch chan int) {
+	for {
+		fmt.Println(<-ch);
+	}
+}
+
+
+func main() {
+	ch := make(chan int);
+	go pump(ch);
+	go suck(ch);
+	fmt.Println(<-ch);
+
+	time.Sleep(5 * 1e9);
+}

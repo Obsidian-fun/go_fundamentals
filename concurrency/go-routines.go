1c2170024cacb724b8e686b193358ad27d0828d4
Obsidian <obsidiancrystal@tutanota.com>
2024-06-14 16:51:20 +0530

    modified:   go-routines.go

--- concurrency/go-routines.go
+++ concurrency/go-routines.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+// time.Sleep() operated in nano seconds mo a second is time.Sleep(1e9).
+
 func main() {
 	fmt.Prinln("In main()");
 
@@ -14,5 +16,12 @@ func main() {
 
 func longWait() {
 	fmt.Println("Beginning long wait");
+	time.Sleep(5 * 1e9);
+	fmt.Println("End of long wait");
+}
 
+func shortWait() {
+	fmt.Println("Beginning of short wait");
+	time.Sleep(2 * 1e9);
+	fmt.Println("End of short wait");}
 }

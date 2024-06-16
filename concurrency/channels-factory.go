aa046ecd81804c2ebf7306de327cf054700fe3a2
Obsidian <obsidiancrystal@tutanota.com>
2024-06-16 17:51:29 +0530

    modified:   channels-factory.go

--- concurrency/channels-factory.go
+++ concurrency/channels-factory.go
@@ -13,6 +13,27 @@ import (
 
 func main() {
 
+	stream := pump();
+	go suck(stream);
 
+	time.Sleep(1e9);
 }
 
+func pump() chan int{
+
+	ch:=make(chan int);
+
+	go func() {
+		for i:=0; i<10; i++ {
+			ch <- i;
+		}
+	}();
+
+	return ch;
+}
+
+func suck(ch chan int) {
+	for i:=0; i<10; i++ {
+		fmt.Println(<-ch);
+	}
+}

f2033d0a0cacac47f8d1d2acf1cf9b7d83905ad5
Obsidian <obsidiancrystal@tutanota.com>
2024-06-20 21:04:22 +0530

    modified:   simple-timeout.go

--- concurrency/timeout/simple-timeout.go
+++ concurrency/timeout/simple-timeout.go
@@ -1,29 +1,42 @@
 /***
-
+Here, the ch channel keeps a track of the computation, if the computation takes longer
+than 1 second, the timeout channel halts the program.
 
 ***/
 
 package main
 
 import (
-//	"fmt"
+	"fmt"
 	"time"
 )
 
 func main() {
 
-	ch := make(chan int);
+	ch := make(chan int, 10);
 	timeout := make(chan bool, 1);
 
 	go func(){
+
+		for i:=0; i<10;i++ {
+			time.Sleep(time.Millisecond*100);
+			ch<-i;
+		}
 		time.Sleep(1e9);
 		timeout <- true;
 	}()
 
-	select {
-		case <-ch:
-		case <-timeout:
-			break;
+	for {
+		select {
+			case value:=<-ch:
+				fmt.Println(value);
+
+			case end:=<-timeout:
+				if end == true {
+					fmt.Println("Finito");
+					return;
+				}
+		}
 	}
 }
 

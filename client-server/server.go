f8fcc0b1fb51f77b0fd6ae74c2f8887e2141e88c
Obsidian <obsidiancrystal@tutanota.com>
2024-06-21 15:56:22 +0530

    modified:   server.go

--- client-server/server.go
+++ client-server/server.go
@@ -7,8 +7,22 @@ import (
 )
 
 func main(){
+	fmt.Println("Starting the server...");
+	listener, err := net.Listen("tcp",":8000");
+	if err != nil {
+		fmt.Println("error : ", err);
+		return
+	}
+
+	for {
+		conn, err := listener.Accept();
+		if err != nil {
+			fmt.Println("error connecting, ", err);
+			return
+		}
 
 
+	}
 }
 
 

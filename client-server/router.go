48fb02653228bb96db9e88d24525a2b61a728dc4
Obsidian <obsidiancrystal@tutanota.com>
2024-06-22 23:58:22 +0530

    modified:   router.go

--- client-server/router.go
+++ client-server/router.go
@@ -6,4 +6,22 @@ Credit: https://codesalad.dev/blog/how-to-build-a-go-router-from-scratch-3
 
 package main
 
+import (
+	"net/http"
+)
+
+type Router struct{}
+
+func (sr *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	http.NotFound(w, r);
+}
+
+func main() {
+	r := &Router{};
+	http.ListenAndServe(":8000",r);
+}
+
+
+
+
 

32cc7ad58dae69ccefba6757800ccfe3a052e6fc
Obsidian <obsidiancrystal@tutanota.com>
2024-06-22 23:39:27 +0530

    modified:   generator.go

--- concurrency/generator.go
+++ concurrency/generator.go
@@ -11,6 +11,20 @@ import (
 	"fmt"
 )
 
+var resume chan int;
+
+func integers() chan int {
+	yield := make(chan int);
+	count := 0;
+	go func() {
+		for {
+			yield <- count;
+			count++;
+		}
+	}()
+	return yield;
+}
+
 func main() {
 
 

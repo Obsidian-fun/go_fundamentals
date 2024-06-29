611725864154422acaaf2e3980351d4823b61989
Obsidian <obsidiancrystal@tutanota.com>
2024-06-29 23:56:01 +0530

    modified:   csv.go

--- encoding/csv.go
+++ encoding/csv.go
@@ -3,11 +3,22 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"io"
 	"encoding/csv"
 )
 
 func main() {
+	r := csv.NewReader(strings.NewReader("a,b,c\ne,f,g\n1,2,3"));
 
+	for {
+		r, err := r.Read(); if err == io.EOF {break};
 
+		if err != nil {
+			fmt.Println("error: ", err);
+			break;
+		}
+		fmt.Println(r);
+	}
 }
 

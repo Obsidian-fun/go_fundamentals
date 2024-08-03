cdf62bd71e3cc7f91c29606a1075cc0a8af86ac2
Obsidian-fun <obsidiancrystal@tutanota.com>
2024-08-03 21:23:45 +0530

    modified:   early-stop-scan.go

---
+++ files/scanner/early-stop-scan.go
@@ -0,0 +1,27 @@
+/***
+Source: go docs, "pkg.go.dev/bufio"
+***/
+
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"strings"
+	"bytes"
+	"os"
+)
+
+func main() {
+	onComma := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+									
+
+	}
+
+	scanner := bufio.NewScanner();
+
+
+}
+
+
+

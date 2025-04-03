09cf244084e4b1b42d34030438d92af1000a2c28
Obsidian-fun <obsidiancrystal@tutanota.com>
2025-04-03 21:50:37 +0530

    modified:   struct_padding.go

--- optimizations/struct_padding.go
+++ optimizations/struct_padding.go
@@ -1,5 +1,9 @@
 /***
+Struct padding: Go compilers may pad some bytes
+after certain fields of struct values. The padded bytes are counted for struct sizes. So the size of a
+struct type may be not a simple sum of the sizes of all its fields.
 
+Go Optimizations 101, pg 12
 
 ***/
 
@@ -11,9 +15,14 @@ import (
 	"unsafe"
 )
 
+type T1 struct {
+	a int32;
+}
+
+
 func main() {
 
-
+	fmt.Println(unsafe.Sizeof(T1{}));
 
 }
 

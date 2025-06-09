3608c80b7d07e72ff8f90834f114b3d7225303b5
Obsidian-fun <obsidiancrystal@tutanota.com>
2025-06-09 13:20:37 +0530

    modified:   hft.go 	renamed:    generics.go -> pitfalls.go

---
+++ sync/hft.go
@@ -0,0 +1,28 @@
+/***
+using sync.Pool in HFT as a light example
+
+***/
+
+package main
+
+import (
+	"sync"
+)
+
+type Order struct {
+	ID string;
+	Amount float64;
+}
+
+var orderPool = sync.Pool {
+	New: func() interface{} {
+		return &Order{}
+	},
+}
+
+
+
+func main() {
+
+}
+
--- sync/pitfalls.go
+++ sync/pitfalls.go
@@ -1,7 +1,10 @@
 /***
-# Type assetion overhead:
-
+Common pitfalls to using sync.Pool
+#1 Type assetion overhead:
 using raw interface{} can be error prone compared to using generics.
+#2 Storing large objects
+only use for frequently use small to medium sized objects.
+for large objects use github.com/fatih/pool
 
 ***/
 
